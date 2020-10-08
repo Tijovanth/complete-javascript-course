cabf64988b9da9824dd140b339caada8079146f9
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-10-09 01:26:51 +0530

    Update Script.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,58 @@
 console.log("Hello Tijovanth you are ready to learn javascript. Focus on your studies. Crack zoho or Freshworks");
+
+
+/* *********************
+Variables and Data Types
+============
+var firstName = "Tijovanth";
+console.log(firstName);
+
+var age  = 3;
+console.log(age);
+
+var isFirstName = true;
+console.log(isFirstName);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+//Rules for variables in Js
+//variable name should not start with numbers or any special characters expect underscore or dollor
+// variable name should not be reserved keyword
+
+// var 3years = 3 it will throw error
+// var if = 3
+
+var _fullName = "Tijovanth";
+var $myName = "Tijovanth";
+console.log(_fullName,$myName);
+*/
+
+/* *********************
+Variables Mutation and Type and Coercion
+================
+
+
+var firstName = "tijo";
+var age = 21
+
+//Type coercion
+console.log(firstName+" "+age);
+
+var job, isMarried;
+job = 'Developer';
+isMarried = false
+
+console.log(firstName+" is a "+age +" year old "+job+" is he married? "+isMarried);
+job = "Architect";
+// variable mutation
+age = "twenty one";
+console.log(firstName+" is a "+age +" year old "+job+" is he married? "+isMarried);
+// alert function 
+alert(firstName+" is a "+age +" year old "+job+" is he married? "+isMarried);
+var lastname = prompt("What is his last name?");
+console.log("lastName is "+ " " + lastname);
+*/
