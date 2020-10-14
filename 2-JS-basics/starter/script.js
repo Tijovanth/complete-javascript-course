f63b4814bece3cc2aaa3d35e11183b7aa548525d
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-10-14 23:27:21 +0530

    Update script.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -56,3 +56,109 @@ alert(firstName+" is a "+age +" year old "+job+" is he married? "+isMarried);
 var lastname = prompt("What is his last name?");
 console.log("lastName is "+ " " + lastname);
 */
+
+/* *********************
+Basic Operators
+================
+var year,yearJohn,yearMark
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn,yearMark);
+console.log(window.now)
+console.log(window.ageJohn);
+console.log(window.ageMark);
+
+// Logical Operator
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older tha John');
+var x;
+console.log(typeof x);
+*/
+
+/*****************************
+* Operator precedence
+*/
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+
+// More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x--;
+console.log(x);
+*/
+
+/*****************************
+* CODING CHALLENGE 1
+*/
+
+// var massMark = 78; // kg
+// var heightMark = 1.69; // meters
+
+// var massJohn = 92;
+// var heightJohn = 1.95;
+
+// var BMIMark = massMark / (heightMark * heightMark);
+// var BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
