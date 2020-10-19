e9d0561335980e80b063b775cfa2bbf8fdaadf37
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-10-20 02:45:33 +0530

    Update app.js

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,7 +15,7 @@ score = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector('.dice').style.display = none;
+document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = 0;
 document.getElementById('current-0').textContent = 0;
@@ -28,4 +28,31 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     diceDom.style.display = 'block';
     var dice = Math.floor(Math.random() * 6) + 1;
     diceDom.src = 'dice-' + dice + '.png';
+
+    if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+       nextPlayer();
+    }
 });
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    score[activePlayer] += roundScore;
+    document.querySelector('#score-' +activePlayer).textContent = scores[activePlayer];
+    if(score[activePlayer] >= 100){
+        
+    }
+    nextPlayer();
+});
+
+function nextPlayer(){
+
+    activePlayer =  activePlayer === 0 ? 1 : 0;
+       roundScore = 0;
+       document.getElementById('current-0').textContent = 0;
+       document.getElementById('current-1').textContent = 0;
+       document.querySelector('.player-0-panel').classList.toggle('active');
+       document.querySelector('.player-1-panel').classList.toggle('active');
+       document.querySelector('.dice').style.display = 'none';
+}
