c0aa82599bbf3df04ed2009914b10b16daf16e0e
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-10-20 23:21:53 +0530

    Update app.js

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,18 +9,24 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer
+var scores, roundScore, activePlayer
 
-score = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
+init();
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+}
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
 
@@ -38,13 +44,19 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
 });
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
-    score[activePlayer] += roundScore;
+    scores[activePlayer] += roundScore;
     document.querySelector('#score-' +activePlayer).textContent = scores[activePlayer];
-    if(score[activePlayer] >= 100){
-        
-    }
+    if(scores[activePlayer] >= 20){
+        document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+    }else{
     nextPlayer();
+    }
 });
+
+document.querySelector('.btn-new').addEventListener('click',init);
 
 function nextPlayer(){
 
